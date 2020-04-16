7236873a99bfe38b32df57658e346bb178ba5819
rendong <rzhang6@uvm.edu>
2020-04-16 12:04:31 -0400

    data archiver

--- IndividualApp/ComposeViewController.swift
+++ IndividualApp/ComposeViewController.swift
@@ -16,6 +16,7 @@ class ComposeViewController: UIViewController {
     
     @IBOutlet weak var yearTF: UITextField!
     
+    var dataStore = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,10 +38,20 @@ class ComposeViewController: UIViewController {
         companyTF.resignFirstResponder()
         yearTF.resignFirstResponder()
         let item = [nameTF.text!, salesTF.text!, companyTF.text!, yearTF.text!]
+        if let arr = UserDefaults.standard.value(forKey: "vgArrs")as? [[String]]{
+            var allVGs = arr
+            allVGs.append(item)
+            dataStore.set(allVGs, forKey: "vgArrs")
+        }else{
+            var allVGs = vg
+            allVGs.append(item)
+            dataStore.set(allVGs, forKey: "vgArrs")
+        }
         vg.append(item)
         self.navigationController?.popViewController(animated: true)
         let vgViewController = self.navigationController?.topViewController as? VideoGameViewController
         vgViewController?.tableView?.reloadData()
+        
         
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
--- IndividualApp/VideoGameViewController.swift
+++ IndividualApp/VideoGameViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class VideoGameViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
-  
+    var dataStore = UserDefaults.standard
     
     override func viewDidLoad() {
         let itemArchiveURL: URL = {
@@ -21,49 +21,61 @@ class VideoGameViewController: UIViewController,UITableViewDelegate,UITableViewD
         return documentDirectory.appendingPathComponent("items.archive")
         }()
         super.viewDidLoad()
+        
         self.tableView.reloadData()
 
         // Do any additional setup after loading the view.
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return vg.count
+        var vgArray = UserDefaults.standard.value(forKey: "vgArrs")as! [[String]]
+        return vgArray.count
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+            var vgArray = UserDefaults.standard.value(forKey: "vgArrs")as! [[String]]
         let cell = tableView.dequeueReusableCell(withIdentifier: "VideoGameCell", for: indexPath) as! Cell
-        cell.Name.text = vg[indexPath.row][0]
-        cell.NumPlayers.text = vg[indexPath.row][1] 
+        cell.Name.text = vgArray[indexPath.row][0]
+        cell.NumPlayers.text = vgArray[indexPath.row][1]
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var vgArray = UserDefaults.standard.value(forKey: "vgArrs")as! [[String]]
         let Storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = Storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-        vc.getName = vg[indexPath.row][0]
-        vc.getSales = vg[indexPath.row][1]
+        vc.getName = vgArray[indexPath.row][0]
+        vc.getSales = vgArray[indexPath.row][1]
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let movedObj = vg[sourceIndexPath.item]
-        vg.remove(at: sourceIndexPath.item)
-        vg.insert(movedObj, at: destinationIndexPath.item)
+        var vgArray = UserDefaults.standard.value(forKey: "vgArrs")as! [[String]]
+        let movedObj = vgArray[sourceIndexPath.item]
+        vgArray.remove(at: sourceIndexPath.item)
+        vgArray.insert(movedObj, at: destinationIndexPath.item)
+        dataStore.set(vgArray, forKey: "vgArrs")
+        
     }
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        var vgArray = UserDefaults.standard.value(forKey: "vgArrs")as! [[String]]
         if editingStyle == .delete{
             // ask user to confirm
-            let item = vg[indexPath.row]
+            let item = vgArray[indexPath.row]
             let title = "Delete \(item[0])?"
             let message = "Are you sure you want to delete this item?"
             let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
             ac.addAction(cancelAction)
             let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { (action) -> Void in
-            // remove the item from the store
-            vg.remove(at: indexPath.item)
-            tableView.deleteRows(at: [indexPath], with: .automatic)
+                // remove the item from the store
+                vgArray.remove(at: indexPath.item)
+                self.dataStore.set(vgArray, forKey: "vgArrs")
+                tableView.deleteRows(at: [indexPath], with: .automatic)
             
             })
+            
             ac.addAction(deleteAction)
             // put up the controller as a modal view
+          
             present(ac, animated: true, completion: nil)
             
 //            vg.remove(at: indexPath.item)
