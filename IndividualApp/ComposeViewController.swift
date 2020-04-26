ce3c25e298f4cba815cf7830d3c184bfaa8cb228
rendong <rzhang6@uvm.edu>
2020-04-25 21:20:46 -0400

    update UI

--- IndividualApp/ComposeViewController.swift
+++ IndividualApp/ComposeViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBOutlet weak var nameTitle: UILabel!
+    @IBOutlet weak var salesTitle: UILabel!
+    @IBOutlet weak var companyTitle: UILabel!
+    @IBOutlet weak var cancelButtonLB: UIButton!
+    @IBOutlet weak var saveButtonLB: UIButton!
+    @IBOutlet weak var yearTitle: UILabel!
     @IBOutlet weak var nameTF: UITextField!
     @IBOutlet weak var salesTF: UITextField!
     @IBOutlet weak var companyTF: UITextField!
@@ -19,6 +25,7 @@ class ComposeViewController: UIViewController {
     var itemStore = ItemStore()
    
     override func viewDidLoad() {
+        UIsetUp()
         super.viewDidLoad()
 
     }
@@ -49,6 +56,59 @@ class ComposeViewController: UIViewController {
         
         
     }
+    func UIsetUp(){
+        nameTitle.backgroundColor = UIColor.white
+        nameTitle.layer.shadowColor = UIColor.lightGray.cgColor
+        nameTitle.layer.shadowOpacity = 1;
+        nameTitle.layer.shadowRadius = 2.0;
+        
+        nameTF.backgroundColor = UIColor.white
+        nameTF.layer.shadowColor = UIColor.lightGray.cgColor
+        nameTF.layer.shadowOpacity = 1;
+        nameTF.layer.shadowRadius = 2.0;
+        
+        salesTitle.backgroundColor = UIColor.white
+        salesTitle.layer.shadowColor = UIColor.lightGray.cgColor
+        salesTitle.layer.shadowOpacity = 1;
+        salesTitle.layer.shadowRadius = 2.0;
+        
+        salesTF.backgroundColor = UIColor.white
+        salesTF.layer.shadowColor = UIColor.lightGray.cgColor
+        salesTF.layer.shadowOpacity = 1;
+        salesTF.layer.shadowRadius = 2.0;
+        
+        companyTitle.backgroundColor = UIColor.white
+        companyTitle.layer.shadowColor = UIColor.lightGray.cgColor
+        companyTitle.layer.shadowOpacity = 1;
+        companyTitle.layer.shadowRadius = 2.0;
+        
+        companyTF.backgroundColor = UIColor.white
+        companyTF.layer.shadowColor = UIColor.lightGray.cgColor
+        companyTF.layer.shadowOpacity = 1;
+        companyTF.layer.shadowRadius = 2.0;
+        
+        yearTitle.backgroundColor = UIColor.white
+        yearTitle.layer.shadowColor = UIColor.lightGray.cgColor
+        yearTitle.layer.shadowOpacity = 1;
+        yearTitle.layer.shadowRadius = 2.0;
+        
+        yearTF.backgroundColor = UIColor.white
+        yearTF.layer.shadowColor = UIColor.lightGray.cgColor
+        yearTF.layer.shadowOpacity = 1;
+        yearTF.layer.shadowRadius = 2.0;
+        
+        cancelButtonLB.layer.borderColor = UIColor.systemRed.cgColor
+        cancelButtonLB.layer.borderWidth = 1.5
+        cancelButtonLB.layer.cornerRadius = 16
+        saveButtonLB.layer.borderColor = UIColor.systemGreen.cgColor
+        saveButtonLB.layer.borderWidth = 1.5
+        saveButtonLB.layer.cornerRadius = 16
+        
+        
+        
+        
+    }
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
--- IndividualApp/DetailViewController.swift
+++ IndividualApp/DetailViewController.swift
@@ -19,9 +19,14 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var Sales: UILabel!
     @IBOutlet weak var Company: UILabel!
     @IBOutlet weak var Year: UILabel!
+    @IBOutlet weak var nameTitle: UILabel!
+    @IBOutlet weak var salesTitle: UILabel!
+    @IBOutlet weak var companyTitle: UILabel!
     
+    @IBOutlet weak var yearTitle: UILabel!
     
     override func viewDidLoad() {
+        UIsetUp()
         super.viewDidLoad()
         Name.text! =  getName
         Sales.text! =  getSales
@@ -52,6 +57,52 @@ class DetailViewController: UIViewController {
         
         
       
+    }
+    func UIsetUp(){
+        nameTitle.backgroundColor = UIColor.white
+        nameTitle.layer.shadowColor = UIColor.lightGray.cgColor
+        nameTitle.layer.shadowOpacity = 1;
+        nameTitle.layer.shadowRadius = 2.0;
+        
+        Name.backgroundColor = UIColor.white
+        Name.layer.shadowColor = UIColor.lightGray.cgColor
+        Name.layer.shadowOpacity = 1;
+        Name.layer.shadowRadius = 2.0;
+        
+        salesTitle.backgroundColor = UIColor.white
+        salesTitle.layer.shadowColor = UIColor.lightGray.cgColor
+        salesTitle.layer.shadowOpacity = 1;
+        salesTitle.layer.shadowRadius = 2.0;
+        
+        Sales.backgroundColor = UIColor.white
+        Sales.layer.shadowColor = UIColor.lightGray.cgColor
+        Sales.layer.shadowOpacity = 1;
+        Sales.layer.shadowRadius = 2.0;
+        
+        companyTitle.backgroundColor = UIColor.white
+        companyTitle.layer.shadowColor = UIColor.lightGray.cgColor
+        companyTitle.layer.shadowOpacity = 1;
+        companyTitle.layer.shadowRadius = 2.0;
+        
+        Company.backgroundColor = UIColor.white
+        Company.layer.shadowColor = UIColor.lightGray.cgColor
+        Company.layer.shadowOpacity = 1;
+        Company.layer.shadowRadius = 2.0;
+        
+        yearTitle.backgroundColor = UIColor.white
+        yearTitle.layer.shadowColor = UIColor.lightGray.cgColor
+        yearTitle.layer.shadowOpacity = 1;
+        yearTitle.layer.shadowRadius = 2.0;
+        
+        Year.backgroundColor = UIColor.white
+        Year.layer.shadowColor = UIColor.lightGray.cgColor
+        Year.layer.shadowOpacity = 1;
+        Year.layer.shadowRadius = 2.0;
+        
+        
+        
+        
+        
     }
 
 
